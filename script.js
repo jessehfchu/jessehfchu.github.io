3ae6b04c84c62c26a17c25db3e5fcb8aacada8d3
jessehfchu <38926505+jessehfchu@users.noreply.github.com>
2018-05-02 16:51:19 -0600

    Initial Commit
    First Check-In

---
+++ script.js
@@ -0,0 +1,79 @@
+//addToQueue(new Command(printString, [1000, "Test String 1", 500]));
+//addToQueue(new Command(printNewLine, [1000]));
+//addToQueue(new Command(printString, [3000, "Test String 2", 50]));
+//addToQueue(new Command(printNewLine, [1000]));
+//addToQueue(new Command(printString, [5000, "Test String 3", 5]));
+//addToQueue(new Command(printNewLine, [1000]));
+
+for (var i = 1; i <= 50; i++) {
+  addToQueue(new Command(printString, [0, "Test String " + i, 5]));
+  //addToQueue(new Command(printNewLine, [5]));
+}
+
+//addToQueue("Sooo... I added support for new lines.");
+//addToQueue(newLine);
+//addToQueue("And the little arrow thing to make it look more like a console.");
+//addToQueue(newLine);
+//addToQueue("And I can directly control the delays.");
+//addToQueue(newLine);
+//addToQueue("So it pauses between lines.");
+//addToQueue(newLine);
+//addToQueue(newLine);
+//addToQueue("This is kinda cool now actually.");
+//addToQueue(newLine);
+//addToQueue("I wonder what I can build with this...");
+//addToQueue(newLine);
+
+Array.prototype.shuffle = function() {
+    var input = this;
+
+    for (var i = input.length-1; i >=0; i--) {
+
+        var randomIndex = Math.floor(Math.random()*(i+1));
+        var itemAtIndex = input[randomIndex];
+
+        input[randomIndex] = input[i];
+        input[i] = itemAtIndex;
+    }
+    return input;
+}
+
+// Bogosort Hello World
+var msg = Array.from("Hello World").shuffle();
+var attempt = 0;
+var highscore = checkMSG(msg);
+var closest = msg.join("");
+
+//var bgsHW = setTimeout(rollHW,10);
+
+function rollHW() {
+  attempt += 1;
+  var check = checkMSG(msg);
+  closest = check > highscore ? msg.join("") : closest;
+  highscore = check > highscore ? check : highscore;
+  addToQueue("Attempt: " + attempt + " " + msg.join("") + "(" + check + ")" + "   Best: " + closest + "(" + highscore + ")");
+  addToQueue(newLine)
+  if (highscore != 11) {
+    msg.shuffle();
+    setTimeout(rollHW,10);
+    //clearInterval(bgsHW);
+  }
+  //msg.shuffle();
+}
+
+function checkMSG(text) {
+  var score = 0;
+  score += text[0] == "H" ? 1 : 0;
+  score += text[1] == "e" ? 1 : 0;
+  score += text[2] == "l" ? 1 : 0;
+  score += text[3] == "l" ? 1 : 0;
+  score += text[4] == "o" ? 1 : 0;
+  score += text[5] == " " ? 1 : 0;
+  score += text[6] == "W" ? 1 : 0;
+  score += text[7] == "o" ? 1 : 0;
+  score += text[8] == "r" ? 1 : 0;
+  score += text[9] == "l" ? 1 : 0;
+  score += text[10] == "d" ? 1 : 0;
+  //score += text[11] == "!" ? 1 : 0;
+  return score;
+}
---
+++ terminal.js
@@ -0,0 +1,110 @@
+// Constants
+const blinkRate = 500;
+const terminalElementID = "txtCommandLineDisplay";
+const terminalElement = document.getElementById(terminalElementID);
+const userInputElementID = "inputUser";
+const userInputElement = document.getElementById(userInputElementID);
+const displayInputElementID = "txtInputDisplay";
+const displayInputElement = document.getElementById(displayInputElementID);
+
+// Declare Global Command Queue
+var commandQueue = [];
+
+// Start Timers
+var blinkTerminalTimer = setInterval(blinkTerminalCursor, blinkRate);
+var blinkInputTimer = setInterval(blinkInputCursor, blinkRate);
+
+// Start Queue Processor
+processQueue();
+
+// Command Class
+function Command(func, args) {
+  this.func = func;
+  this.args = args;
+}
+
+// Add Command to Queue
+function addToQueue(command) {
+  commandQueue.push(command);
+}
+
+// Execute available Commands, otherwise poll
+function processQueue() {
+  if (commandQueue.length > 0) {
+    let command = commandQueue.shift();
+    command.func.apply(this, command.args);
+  }
+  else {
+    setTimeout(processQueue, 10);
+  }
+}
+
+// Print a string character-by-character
+function printString(delay, msg, speed) {
+  if (msg.length > 0) {
+    let strArray = Array.from(msg);
+    let displayed = terminalElement.innerHTML.replace(/█/, "");
+    terminalElement.innerHTML = displayed + strArray.shift() + "█";
+    setTimeout(printString, speed, delay, strArray.join(""), speed);
+  }
+  else {
+    setTimeout(processQueue, delay);
+  }
+}
+
+// Insert a page break
+function printNewLine(delay) {
+  terminalElement.appendChild(document.createElement("br"));
+  setTimeout(processQueue, delay);
+}
+
+// Blinks Terminal Cursor
+function blinkTerminalCursor() {
+  if (terminalElement.innerHTML.includes("█")) {
+    terminalElement.innerHTML = terminalElement.innerHTML.replace(/█/, "");
+  }
+  else {
+    terminalElement.innerHTML += "█";
+  }
+}
+
+// Blinks Input Cursor
+function blinkInputCursor() {
+  if (displayInputElement.innerHTML.includes("█")) {
+    displayInputElement.innerHTML = displayInputElement.innerHTML.replace(/█/, "");
+  }
+  else {
+    displayInputElement.innerHTML += "█";
+  }
+}
+
+
+// Focuses input
+function focusInput() {
+  userInputElement.focus();
+}
+
+// Sync typed input
+function syncInput(input, e) {
+  displayInputElement.innerHTML = "> " + userInputElement.value + "█";
+}
+
+//http://www.dynamicdrive.com/forums/showthread.php?17450-Emulating-a-terminal-like-caret-with-javascript-and-css
+
+
+/*
+// Scroll page down
+function scrollBottom() {
+
+}
+
+var out = document.getElementById("out");
+var c = 0;
+var add = setInterval(function() {
+    // allow 1px inaccuracy by adding 1
+    var isScrolledToBottom = out.scrollHeight - out.clientHeight <= out.scrollTop + 1;
+    console.log(out.scrollHeight - out.clientHeight,  out.scrollTop + 1);
+    // scroll to bottom if isScrolledToBotto
+    if(isScrolledToBottom)
+      out.scrollTop = out.scrollHeight - out.clientHeight;
+}, 1000);*/
