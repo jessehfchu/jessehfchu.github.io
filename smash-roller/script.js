daad308e153adcb6e191c1d8fa91cbbbff269f5b
jessehfchu <38926505+jessehfchu@users.noreply.github.com>
2018-12-06 13:09:19 -0700

    More Themes, graphic behaviour updated.

--- smash-roller/script.js
+++ smash-roller/script.js
@@ -13,43 +13,43 @@ var listThemes = [
   },
   {
     name: "Ladies Night",
-    desc: "Play as Female characters only."
+    desc: "Play only as Female characters."
   },
   {
     name: "Pokemon Battle",
-    desc: "Play as Pokemon characters only."
+    desc: "Play only as Pokemon characters."
   },
   {
     name: "Hyrule Battle",
-    desc: "Play as Zelda characters only."
+    desc: "Play only as Zelda characters."
   },
   {
     name: "Mushroom Kingdom Battle",
-    desc: "Play as Mario characters only."
+    desc: "Play only as Mario characters."
   },
   {
     name: "Pick Mii!",
-    desc: "Play as Mii Fighters only."
+    desc: "Play only as Mii Fighters."
   },
   {
     name: "Echoes",
-    desc: "Play as Echo characters only."
+    desc: "Play only as Echo characters."
   },
   {
     name: "The Original",
-    desc: "Play as characters from Smash 64 only."
+    desc: "Play only as characters from Smash 64."
   },
   {
     name: "Who's The Baddest?",
-    desc: "Play as Villain characters only."
+    desc: "Play only as Villain characters."
   },
   {
     name: "Sword Fight",
-    desc: "Play as characters with swords only."
+    desc: "Play only as characters with swords."
   },
   {
     name: "Heavyweight Division",
-    desc: "Play as Heavyweight characters only."
+    desc: "Play only as Heavyweight characters."
   },
   {
     name: "Let's Trade!",
@@ -61,15 +61,19 @@ var listThemes = [
   },
   {
     name: "Fisticuffs",
-    desc: "Play as unarmed characters only."
+    desc: "Play only as unarmed characters."
   },
   {
     name: "Cameos!",
-    desc: "Play as third-party characters only."
+    desc: "Play only as third-party characters."
   },
   {
     name: "Shaped Like A Friend",
-    desc: "Play as small & round characters only."
+    desc: "Play only as small & round characters."
+  },
+  {
+    name: "Is It Movember?",
+    desc: "Play only as characters with facial hair."
   },
   // Items
   {
@@ -91,7 +95,7 @@ var listThemes = [
   },
   {
     name: "Squad Strike",
-    desc: "Play Strike Strike."
+    desc: "Play Strike Strike mode."
   },
   {
     name: "Stamina Battle",
@@ -102,21 +106,33 @@ var listThemes = [
     desc: "Team Battle: Humans vs CPUs."
   },
   {
-    name: "Team Amiibo",
+    name: "Team amiibo",
     desc: "Team Battle: Humans vs amiibos."
   },
   {
-    name: "Good & Evil",
+    name: "Good vs Evil",
     desc: "Team Battle: Heros vs Villains."
   },
   // Conditions
   {
     name: "The Floor Is Lava",
-    desc: "Only aerial attacks are permitted."
+    desc: "Players must stay on platforms. (SD Penalty)"
   },
   {
     name: "Ready, Aim, Fire!",
-    desc: "Only projectile attacks are permitted."
+    desc: "Players may only use projectile attacks. (SD Penalty)"
+  },
+  {
+    name: "Aerial Ace",
+    desc: "Players may only use aerial attacks. (SD Penalty)"
+  },
+  {
+    name: "Concrete Shoes",
+    desc: "Players may not jump. (SD Penalty)"
+  },
+  {
+    name: "Sleep On The Haters",
+    desc: "Everyone plays as Jigglypuff. Only KO with Rest. (SD Penalty)"
   }
 ];
 
@@ -124,21 +140,33 @@ function rollTheme() {
   listThemes.shuffle();
   eName.style.color = "grey";
   eDesc.style.color = "grey";
-  displayTheme(0);
+  displayTheme();
 }
 
-function displayTheme(i) {
-  var delay = 1500/listThemes.length;
+function displayTheme(i = 0) {
+  // Determine Speed
+  var proportion = i / listThemes.length;
+  var delay = 250;
+  if (proportion < 0.75) {
+    delay = 1500 / listThemes.length;
+  }
+  else if (proportion < 0.9) {
+    delay = 4500 / listThemes.length;
+  }
+  else if (proportion < 1) {
+    delay = 9000 / listThemes.length;
+  }
+  // Update Display
   if (i < listThemes.length) {
     eName.innerHTML = listThemes[i].name;
     eDesc.innerHTML = listThemes[i].desc;
   }
   else {
-    delay = 250;
+    // Flash Selection
     eName.style.color = eName.style.color == "white" ? "grey" : "white";
     eDesc.style.color = eDesc.style.color == "white" ? "grey" : "white";
   }
-  if (i < listThemes.length + 4) {
+  if (i < listThemes.length + 6) {
     setTimeout(displayTheme, delay, i+1);
   }
 }
