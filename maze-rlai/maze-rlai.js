a881088f3dfb29997a7c1943b7f0c0ff3118b02f
jessehfchu <38926505+jessehfchu@users.noreply.github.com>
2020-03-16 16:17:55 -0600

    Working at console level. It's ugly, but it's working.

--- maze-rlai/maze-rlai.js
+++ maze-rlai/maze-rlai.js
@@ -16,11 +16,11 @@ learned = [[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]
 
 /** Player Starting Position **/
 // Top Left
-current_state = {row:0, col:0};
+currentState = {row:0, col:0};
 
 /** Reward Position **/
 // Bottom Right
-reward_state = {row:4, col:4};
+rewardState = {row:4, col:4};
 
 /** Action Encoding **/
 // North = 0
@@ -33,100 +33,97 @@ alpha = 0.1; // Learning Rate
 gamma = 0.6 // Discount Rate
 epsilon = 0.1 // Exploration Rate
 
-// Return next state based on current state and action
-function get_next_state(state, action) {
+/** Get next state resulting from a state/action pair **/
+function getNextState(state, action) {
   let crow = state.row;
   let ccol = state.col;
   switch(action) {
-    case 0:
-      if (crow == 0 || maze[crow - 1][ccol] == 1) {
+    case 0: // North
+      if (crow == 0 || maze[crow - 1][ccol] == 1) { // North edge or invalid
         return state;
       }
       else {
         return {row: crow - 1, col: ccol};
       }
       break;
-    case 1:
-      if (crow == 4 || maze[crow + 1][ccol] == 1) {
+    case 1: // East
+      if (ccol == 4 || maze[crow][ccol + 1] == 1) { // East edge or invalid
+        return state;
+      }
+      else {
+        return {row: crow, col: ccol + 1};
+      }
+      break;
+    case 2: // South
+      if (crow == 4 || maze[crow + 1][ccol] == 1) { // South edge or invalid
         return state;
       }
       else {
         return {row: crow + 1, col: ccol};
       }
       break;
-    case 2:
-      if (crow == 4 || maze[crow + 1][ccol] == 1) {
+    case 3: // West
+      if (ccol == 0 || maze[crow][ccol - 1] == 1) { // West edge or invalid
         return state;
       }
       else {
-        return {row: crow + 1, col: ccol};
+        return {row: crow, col: ccol - 1};
       }
       break;
-    case 3:
-      break;
     default:
-      return current_state;
+      return currentState;
       break;
   }
 }
 
-// Get Action
-// Get State Max Action
-// Get State Max Reward
-// Get Next State
-// Get Reward
-
-function get_reward(dir) {
-  switch (dir) {
-    case 0:
-      if (current_state.x == 0 || maze[playerx - 1][playery] == 1) {
-        hitWall(dir);
-      }
-      else {
-        playerx -= 1;
-      }
-      break;
-    case 1:
-      if (playery == 4 || maze[playerx][playery + 1] == 1) {
-        hitWall(dir);
-      }
-      else {
-        playery += 1;
-      }
-      break;
-    case 2:
-      if (playerx == 4 || maze[playerx + 1][playery] == 1) {
-        hitWall(dir);
-      }
-      else {
-        playerx += 1;
-      }
-      break;
-    case 3:
-      if (playery == 0 || maze[playerx][playery - 1] == 1) {
-        hitWall(dir);
-      }
-      else {
-        playery -= 1;
-      }
-      break;
-    default:
-      break;
+/** Select an action based on current state and hyperparameters **/
+function getAction(state) {
+    if (Math.random() < epsilon) { // Explore
+    return Math.floor(Math.random() * 4); // Random action
   }
-  console.clear();
-  drawMaze();
+  else { // Exploit
+    return getStateMaxAction(state); // Greedy action
+  }
 }
 
-function hitWall(dir) {
-  learned[playerx][playery][dir] = -10
+/** Return the highest-value action for a given state **/
+function getStateMaxAction(state) {
+  let crow = state.row;
+  let ccol = state.col;
+  return learned[crow][ccol].indexOf(Math.max(...learned[crow][ccol]));
 }
 
+/** Return the highest-value reward for a given state **/
+function getStateMaxReward(state) {
+  let crow = state.row;
+  let ccol = state.col;
+  return Math.max(...learned[crow][ccol]);
+}
+
+/** Get the reward associated with a state/action pair **/
+function getReward(state, action) {
+  let newState = getNextState(state, action);
+  if (newState.row == state.row && newState.col == state.col) {
+    return -10;
+  }
+  else if (newState.row == rewardState.row && newState.col == rewardState.col) {
+    return 10;
+  }
+  else {
+    return -1;
+  }
+}
+
+/** Print out the maze to console **/
 function drawMaze() {
   for (x=0; x<5; x++) {
     line = "";
     for (y=0; y<5; y++) {
-      if (x == playerx && y == playery) {
+      if (x == currentState.row && y == currentState.col) {
         line += " P";
+      }
+      else if (x == rewardState.row && y == rewardState.col) {
+        line += " O";
       }
       else {
         if (maze[x][y] == 0) {
@@ -138,5 +135,73 @@ function drawMaze() {
       }
     }
     console.log(line);
+  }
+}
+
+/** Print out the policy to console **/
+function drawPolicy() {
+  for (x=0; x<5; x++) {
+    line = "";
+    for (y=0; y<5; y++) {
+      if (x == rewardState.row && y == rewardState.col) {
+        line += " O";
+      }
+      else if (maze[x][y] == 0) {
+        let action = getStateMaxAction({row: x, col: y});
+        switch (action) {
+          case 0:
+            line += " ^";
+            break;
+          case 1:
+            line += " >";
+            break;
+          case 2:
+            line += " v";
+            break;
+          case 3:
+            line += " <";
+            break;
+          default:
+            break;
+        }
+      }
+      else {
+        line += " X";
+      }
+    }
+    console.log(line);
+  }
+}
+
+/** Execute one action **/
+function step() {
+  let action = getAction(currentState);
+  let nextState = getNextState(currentState, action);
+  let reward = getReward(currentState, action);
+  let nextReward = getStateMaxReward(nextState);
+  let currentValue = learned[currentState.row][currentState.col][action];
+  learned[currentState.row][currentState.col][action] = ((1 - alpha) * currentValue) + (alpha * (reward + (gamma * nextReward)));
+  currentState.row = nextState.row;
+  currentState.col = nextState.col;
+}
+
+/** Restart Maze **/
+function restart() {
+  currentState = {row:0, col:0};
+}
+
+function learn() {
+  // Execution Loop
+  let steps = 0;
+  let wins = 0;
+  for (let i = 0; i < 1000; i++) {
+    while (!((currentState.row == rewardState.row) && (currentState.col == rewardState.col))) {
+      step();
+      console.log(currentState);
+      steps++;
+    }
+    console.log("Win");
+    restart();
+    wins++;
   }
 }
