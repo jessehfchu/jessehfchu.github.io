5380b7ed2fdaa81902b63fc1400a11ef65fc1e28
jessehfchu <38926505+jessehfchu@users.noreply.github.com>
2020-04-20 15:08:08 -0600

    Temporary Usage

--- blackjack-rlai/blackjack-rlai.js
+++ blackjack-rlai/blackjack-rlai.js
@@ -7,3 +7,25 @@ const themeListID = "txtThemeList";
 const eList = document.getElementById(themeListID);
 const btnID = "btnRoll";
 const eBtn = document.getElementById(btnID);
+
+a = ["K", "S", "T"];
+b = ["T", "P", "O", "G", "R"];
+c = ["H", "S", "E", "I"];
+d = ["O", "A", "D", "C", "N", "E"];
+e = ["P", "N", "O", "A", "C", "T"];
+f = ["O", "H", "N", "E"];
+g = ["E", "R", "L", "N", "C", "O"];
+h = ["E", "R", "O", "I", "M", "L", "H", "T", "P", "S", "A"];
+
+list = [];
+
+a.forEach((la, ia) =>
+b.forEach((lb, ib) =>
+c.forEach((lc, ic) =>
+d.forEach((ld, id) =>
+e.forEach((le, ie) =>
+f.forEach((lf, iff) =>
+g.forEach((lg, ig) =>
+h.forEach((lh, ih) => list.push(la+lb+lc+ld+le+lf+lg+lh)))))))));
+
+eDesc.innerHTML = list.join("<br>");
